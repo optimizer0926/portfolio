e66e9ffb4892704ccc6639b3f2ff1e7ce5ad6c1f
HonestDev0926 <strongpheonix102@gmail.com>
2022-09-05 03:11:37 +0300

    fix config 3

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -46,66 +46,66 @@ const StyledHeroSection = styled.section`
 `;
 
 const Hero = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  const prefersReducedMotion = usePrefersReducedMotion();
+    const [isMounted, setIsMounted] = useState(false);
+    const prefersReducedMotion = usePrefersReducedMotion();
 
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return;
-    }
+    useEffect(() => {
+        if (prefersReducedMotion) {
+            return;
+        }
 
-    const timeout = setTimeout(() => setIsMounted(true), navDelay);
-    return () => clearTimeout(timeout);
-  }, []);
+        const timeout = setTimeout(() => setIsMounted(true), navDelay);
+        return () => clearTimeout(timeout);
+    }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Roger Rodriguez.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
-  const four = (
-    <>
-      <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
-      </p>
-    </>
-  );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
-
-  const items = [one, two, three, four, five];
-
-  return (
-    <StyledHeroSection>
-      {prefersReducedMotion ? (
+    const one = <h1>Hi, my name is</h1>;
+    const two = <h2 className="big-heading">Roger Rodriguez.</h2>;
+    const three = <h3 className="big-heading">I build things for the web.</h3>;
+    const four = (
         <>
-          {items.map((item, i) => (
-            <div key={i}>{item}</div>
-          ))}
+            <p>
+                I’m a software engineer specializing in building exceptional
+                digital experiences. Currently, I’m focused on building accessible, human-centered products
+                at{' '}
+                <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
+                    Upstatement
+                </a>
+                .
+            </p>
         </>
-      ) : (
-        <TransitionGroup component={null}>
-          {isMounted &&
-            items.map((item, i) => (
-              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
-                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
-              </CSSTransition>
-            ))}
-        </TransitionGroup>
-      )}
-    </StyledHeroSection>
-  );
+    );
+    const five = (
+        <a
+            className="email-link"
+            href="https://www.newline.co/courses/build-a-spotify-connected-app"
+            target="_blank"
+            rel="noreferrer">
+            Check out my course!
+        </a>
+    );
+
+    const items = [one, two, three, four, five];
+
+    return (
+        <StyledHeroSection>
+            {prefersReducedMotion ? (
+                <>
+                    {items.map((item, i) => (
+                        <div key={i}>{item}</div>
+                    ))}
+                </>
+            ) : (
+                <TransitionGroup component={null}>
+                    {isMounted &&
+                        items.map((item, i) => (
+                            <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+                            </CSSTransition>
+                        ))}
+                </TransitionGroup>
+            )}
+        </StyledHeroSection>
+    );
 };
 
 export default Hero;
